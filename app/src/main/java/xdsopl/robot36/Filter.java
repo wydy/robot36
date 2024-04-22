8fc46077c87e1e25707fd7d256718e2488406df2
Ahmet Inan <xdsopl@gmail.com>
2024-04-22 17:48:50 +0200

    avoid confusion by adding sample rate

--- app/src/main/java/xdsopl/robot36/Filter.java
+++ app/src/main/java/xdsopl/robot36/Filter.java
@@ -14,8 +14,8 @@ public final class Filter {
 		return Math.sin(x) / x;
 	}
 
-	public static double lowPass(double cutoff, int n, int N) {
-		double f = 2 * cutoff;
+	public static double lowPass(double cutoff, double rate, int n, int N) {
+		double f = 2 * cutoff / rate;
 		double x = n - (N - 1) / 2.0;
 		return f * sinc(f * x);
 	}
